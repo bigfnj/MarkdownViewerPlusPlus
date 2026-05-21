95972224c061a1cc1b83f30ed4282c61b0b0df8d
bigfnj <peshinator@gmail.com>
2026-05-21 13:05:00 -0700

    release: bump version 1.0.0 -> 1.1.0

--- MarkdownPlusPlus.Native/src/PluginController.cpp
+++ MarkdownPlusPlus.Native/src/PluginController.cpp
@@ -403,7 +403,7 @@ void PluginController::PrintPreview() {
 void PluginController::ShowAbout() {
     MessageBoxW(
         nppData_._nppHandle,
-        L"Markdown++ native preview\nVersion 1.0.0\n\nRenderer: WebView2\nMarkdown engine: cmark-gfm\nMermaid: bundled/offline\nExports: HTML, PDF, clipboard, print",
+        L"Markdown++ native preview\nVersion 1.1.0\n\nRenderer: WebView2\nMarkdown engine: cmark-gfm\nMermaid: bundled/offline\nExports: HTML, PDF, clipboard, print",
         kPluginName,
         MB_OK | MB_ICONINFORMATION);
 }
